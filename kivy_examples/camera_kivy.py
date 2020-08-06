d1ea4d7377f118c8c072c4cdb19025bd50a7b687
iwaniec <iwaniec@gmail.com>
2020-08-06 14:49:51 -0700

    camera working in cv2 + kivy

--- kivy_examples/camera_kivy.py
+++ kivy_examples/camera_kivy.py
@@ -1,9 +1,10 @@
+# coding:utf-8
 from kivy.app import App
 from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 import cv2
-from kivy.uix.camera import Camera
+
 
 class KivyCamera(Image):
     def __init__(self, capture, fps, **kwargs):
@@ -17,7 +18,8 @@ class KivyCamera(Image):
             # convert it to texture
             buf1 = cv2.flip(frame, 0)
             buf = buf1.tostring()
-            image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture = Texture.create(
+                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
             image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
             # display image from the texture
             self.texture = image_texture
@@ -32,3 +34,7 @@ class CamApp(App):
     def on_stop(self):
         #without this, app will not exit even if the window is closed
         self.capture.release()
+
+
+if __name__ == '__main__':
+    CamApp().run()
