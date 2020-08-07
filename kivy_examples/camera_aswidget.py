6544baa297548af94618b28b9a62829a726c299c
iwaniec <iwaniec@gmail.com>
2020-08-06 18:36:09 -0700

    trying to make camera as a widget

--- kivy_examples/camera_aswidget.py
+++ kivy_examples/camera_aswidget.py
@@ -1,34 +1,44 @@
-import kivy
-kivy.require('1.7.2')
-
+# coding:utf-8
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.camera import Camera
-from kivy.uix.button import Button
-from kivy.core.window import Window
+from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+import cv2
+
+class MyWindow(BoxLayout):
+    pass
+
+class KivyCamera(Image):
+    def __init__(self, capture, fps, **kwargs):
+        super(KivyCamera, self).__init__(**kwargs)
+        self.capture = capture
+        Clock.schedule_interval(self.update, 1.0 / fps)
+
+    def update(self, dt):
+        ret, frame = self.capture.read()
+        if ret:
+            # convert it to texture
+            buf1 = cv2.flip(frame, 0)
+            buf = buf1.tostring()
+            image_texture = Texture.create(
+                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            # display image from the texture
+            self.texture = image_texture
+
 
 class CamApp(App):
-          # Function to take a screenshot
-          def screengrab(self,*largs):
-                outname = self.fileprefix+'_%(counter)04d.png'
-                Window.screenshot(name=outname)
+    def build(self):
+        self.capture = cv2.VideoCapture(0)
+        self.my_camera = KivyCamera(capture=self.capture, fps=30)
+        #return self.my_camera
+        return MyWindow()
 
-          def build(self):
+    def on_stop(self):
+        #without this, app will not exit even if the window is closed
+        self.capture.release()
 
-                # create a floating layout as base
-                camlayout = FloatLayout(size=(600, 600))
-                cam = Camera()        #Get the camera
-                cam=Camera(resolution=(1024,1024), size=(300,300))
-                cam.play=True         #Start the camera
-                camlayout.add_widget(cam)
-
-                button=Button(text='Take Picture',size_hint=(0.12,0.12))
-                button.bind(on_press=self.screengrab)
-                camlayout.add_widget(button)    #Add button to Camera Layout
-
-                self.fileprefix = 'snap'
-
-                return camlayout
 
 if __name__ == '__main__':
     CamApp().run()
