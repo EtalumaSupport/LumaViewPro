c9a45422445d7908f45331fc5f28efb301f864ad
iwaniec <iwaniec@gmail.com>
2020-08-14 19:20:43 -0700

    played with padding and spacing

--- lumaviewpro.py
+++ lumaviewpro.py
@@ -92,7 +92,7 @@ void main (void) {
   frag_color = color;
   tex_coord0 = vTexCoords0;
   gl_Position = projection_mat * modelview_mat * vec4(vPosition.xy, 0.0, 1.0);
-}
+# }
 ''')
 
     viewer = ObjectProperty(None)
