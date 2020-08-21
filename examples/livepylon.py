f93b0c5fc65cf6ae1da7656a9713d71e3f8758d8
iwaniec <iwaniec@gmail.com>
2020-08-21 10:13:12 -0700

    took out the while loop. AWESOME! Need to fix the texture format.

--- examples/livepylon.py
+++ examples/livepylon.py
@@ -15,24 +15,24 @@ class PylonCam(Image):
         self.camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
 
     def play(self):
-        while self.camera.IsGrabbing():
+        # while self.camera.IsGrabbing():
             # Wait for an image and then retrieve it. A timeout of 500 ms is used.
-            img = self.camera.RetrieveResult(500, pylon.TimeoutHandling_ThrowException)
-            if img.GrabSucceeded():
-                # returns a numpy array in the shape of the image
-                img_array = img.GetArray()
-                 # creates a 1D array of type uint8
-                # img_string = img_array.tostring() # equivalent
-                img_string = img_array.reshape(-1,)
-                img_string = img_array.flatten()
-                print(img_string[0])
-                # Works up to here...
+        img = self.camera.RetrieveResult(500, pylon.TimeoutHandling_ThrowException)
+        if img.GrabSucceeded():
+            # returns a numpy array in the shape of the image
+            img_array = img.GetArray()
+             # creates a 1D array of type uint8
+            # img_string = img_array.tostring() # equivalent
+            img_string = img_array.reshape(-1,)
+            img_string = img_array.flatten()
+            print(img_string[0])
+            # Works up to here...
 
-                # create a texture that has the shape of the image
-                texture = Texture.create(size=img_array.shape, colorfmt='luminance')
-                # buffer the 1D array into the texture
-                texture.blit_buffer(img_string, colorfmt="luminance", bufferfmt='ubyte')
-                self.texture = texture
+            # create a texture that has the shape of the image
+            texture = Texture.create(size=img_array.shape, colorfmt='luminance')
+            # buffer the 1D array into the texture
+            texture.blit_buffer(img_string, colorfmt="luminance", bufferfmt='ubyte')
+            self.texture = texture
 
 # BoxLayout containing Camera and Button
 class MainBox(BoxLayout):
