b0fe2da311241752bb6b77f3274986aeccd96d24
iwaniec <iwaniec@gmail.com>
2020-08-20 23:37:44 -0700

    attempt at pushing pylon into kivy - LivePylon.py

---
+++ examples/livepylon.py
@@ -0,0 +1,48 @@
+# imports
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+from pypylon import pylon
+import numpy as np
+import cv2
+
+
+class PylonCam(Image):
+
+    def __init__(self, **kwargs):
+        super(PylonCam, self).__init__(**kwargs)
+
+        self.camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+        self.camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+
+        while self.camera.IsGrabbing():
+            # Wait for an image and then retrieve it. A timeout of 500 ms is used.
+            img = self.camera.RetrieveResult(500, pylon.TimeoutHandling_ThrowException)
+
+            if img.GrabSucceeded():
+                # returns a numpy array in the shape of the image
+                img_array = img.GetArray()
+                 # creates a 1D string in uint8 format
+                img_string = img_array.tostring()
+                print(img_string[0])
+                # Works up to here...
+
+                # create a texture that has the shape of the image
+                texture = Texture.create(size=img_array.shape, colorfmt='luminance')
+                # buffer the 1D array into the texture
+                texture.blit_buffer(img_string, colorfmt="luminance", bufferfmt='ubyte')
+                self.texture = texture
+
+# BoxLayout containing Camera and Button
+class MainBox(BoxLayout):
+    pass
+
+# Boxlayout is the App class
+class LivePylonApp(App):
+   def build(self):
+       return MainBox()
+
+
+# Instantiate and run the kivy app
+LivePylonApp().run()
