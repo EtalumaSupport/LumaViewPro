cdcdad51f8ac8c38045c8ab9252f80b75cfb0f0f
Dr. Kevin Peter Hickerson <hickerson@gmail.com>
2021-07-04 12:38:20 -0700

    Made changes that will probably conflict

--- sandbox/shadercode.py
+++ sandbox/shadercode.py
@@ -66,10 +66,10 @@ class PylonCamera(Image):
         if self.camera == False:
             self.connect()
             if self.camera == False:
-                #self.source = "~/Documents/Earthineering/isim_optim.jpg"
+                self.source = "~/Documents/Earthineering/isim_optim.jpg"
                 #self.source = "~/Documents/Earthineering/DaneRecolor_optim.jpg"
                 #self.source = "~/Documents/Earthineering/McNamara2_optim.jpg"
-                self.source = "./data/camera to USB.png"
+                #self.source = "./data/camera to USB.png"
                 return
         try:
             if self.camera.IsGrabbing():
