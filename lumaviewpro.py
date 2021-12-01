83de63474d5df683d527f2eb84fdb0723335bb55
Anna Hickerson <iwaniec@gmail.com>
2021-12-01 15:10:59 -0800

    typo correction

--- lumaviewpro.py
+++ lumaviewpro.py
@@ -256,7 +256,7 @@ class PylonCamera(Image):
 class MainDisplay(FloatLayout):
     led_board = ObjectProperty(None)
     led_board = LEDBoard()
-    protocol['port'] = led_board.device
+    protocol['port'] = led_board.port
 
     motion = ObjectProperty(None)
     motion = TrinamicBoard()
