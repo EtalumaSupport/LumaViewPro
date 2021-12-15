c0fde48d755238b9ffa4a093b652d529eb3b71a8
Anna Hickerson <iwaniec@gmail.com>
2021-12-15 07:12:11 -0800

    improved script exampe

--- script_example.py
+++ script_example.py
@@ -20,10 +20,11 @@ led = LEDBoard()
 xyz = TrinamicBoard()
 cam = PylonCamera()
 
+'''
 # ----------------------------------------------------
 # Controlling an LED
 # ----------------------------------------------------
-led.led_on(0, 100)  # turn on LED at channel 0 at 100mA
+led.led_on(0, 50)  # turn on LED at channel 0 at 50mA
 time.sleep(1)       # wait one second
 led.led_off()       # turn off all LEDs
 
@@ -32,13 +33,44 @@ led.led_off()       # turn off all LEDs
 # ----------------------------------------------------
 xyz.zhome()         # home position (retracted) objective
 xyz.xyhome()        # home position of xy stage
-xyz.move_abs_pos('Z', 1000)    # move to absolute position at 1000um (1 mm)
+xyz.move_abs_pos('X', 5800)    # move to absolute position in um
+xyz.move_abs_pos('Y', 3500)    # move to absolute position in um
+xyz.move_abs_pos('Z', 3270)    # move to absolute position in um
 
 # ----------------------------------------------------
 # Controlling the Camera
 # ----------------------------------------------------
 if cam.active:
     cam.frame_size(1900,1900)
-    array = cam.array
-    img = Image.fromarray(array)
+    cam.grab()
+    img = Image.fromarray(cam.array)
     img.show()
+'''
+
+
+
+# ----------------------------------------------------
+# Example
+# ----------------------------------------------------
+if cam.active:
+    cam.frame_size(1900,1900)
+
+    led.led_on(0, 25)  # turn on LED at channel 0 at 50mA
+    time.sleep(1)       # wait 1 sec
+    cam.grab()
+    img = Image.fromarray(cam.array)
+    img.show()
+
+    led.led_on(1, 50)  # turn on LED at channel 0 at 50mA
+    time.sleep(1)       # wait 1 sec
+    cam.grab()
+    img = Image.fromarray(cam.array)
+    img.show()
+
+    led.led_on(2, 150)  # turn on LED at channel 0 at 50mA
+    time.sleep(1)       # wait 1 sec
+    cam.grab()
+    img = Image.fromarray(cam.array)
+    img.show()
+
+    led.led_off()       # turn off all LEDs
