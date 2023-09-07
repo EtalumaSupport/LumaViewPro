0dd9a61da4209d733afda37185bd0366835b97fd
Jordan <jmcoreymv@hotmail.com>
2023-09-07 14:44:39 -0700

    Enable console

--- scripts/build_win_release.ps1
+++ scripts/build_win_release.ps1
@@ -6,7 +6,6 @@ $repo_url = "https://github.com/jmcoreymv/LumaViewPro-Private.git"
 $branch = "feature/add-windows-exe-gen"
 
 $version = Read-Host -Prompt "Set version"
-Write-Host "Version: $version"
 $lvp_base_w_version = "LumaViewPro-$version"
 
 $starting_dir = Get-Location
@@ -22,12 +21,12 @@ New-Item -Path $working_dir -ItemType Directory
 New-Item -Path $repo_dir -ItemType Directory
 New-Item -Path $artifact_dir -ItemType Directory
 
-echo "Cloning $branch for release"
+echo "Cloning $repo_url@$branch for release"
 git clone --depth 1 --branch $branch $repo_url $repo_dir
 Set-Location -Path $repo_dir
 
-echo "Creating release directory"
-New-Item -Path '.\release' -ItemType Directory
+# echo "Creating release directory"
+# New-Item -Path '.\release' -ItemType Directory
 
 echo "Adding license files to top-level"
 Copy-Item '.\licenses\*' -Destination '.\' -Force
