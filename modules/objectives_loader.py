c7010f6987ddaa6a64bee635ec81653ca89db6d7
Jordan <jmcoreymv@hotmail.com>
2024-04-04 08:08:26 -0700

    Add objective loader and cleanup objective data handling

---
+++ modules/objectives_loader.py
@@ -0,0 +1,52 @@
+
+'''
+MIT License
+
+Copyright (c) 2023 Etaluma, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyribackground_downght notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
+
+```
+This open source software was developed for use with Etaluma microscopes.
+'''
+
+import json
+
+from lvp_logger import logger
+
+class ObjectiveLoader:
+
+    def __init__(self, *arg):
+
+        with open('./data/objectives.json', "r") as read_file:
+            self._objectives = json.load(read_file)
+        
+
+    def get_objective_info(self, objective_id: str) -> dict:
+        try:
+            objective_info = self._objectives[objective_id]
+        except:
+            raise Exception(f"Unable to retrieve information for objective {objective_id}")
+        
+        return objective_info
+    
+
+    def get_objectives_list(self) -> list:
+        return list(self._objectives.keys())
