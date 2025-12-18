b2d7e53401cb3f592deba4a36e7e4f6f48ffd3ee
Daniel Book <daniel@bookfamily.com>
2025-12-18 13:12:06 -0800

    combined /move/absolute and /move/relative into /move using pydantic models for data validation

--- rest_api/api_v1/motion_router.py
+++ rest_api/api_v1/motion_router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from rest_api.api_v1.api_config import get_scope
 from lumascope_api import Lumascope
 from enum import Enum
+from pydantic import BaseModel
 
 motion_router = APIRouter(prefix="/move", tags=['Motion'])
 
@@ -11,18 +12,30 @@ class Axis(str, Enum):
     Z = "Z"
     T = "T"
 
-@motion_router.post("/absolute")
-async def move_absolute(axis: Axis, pos: float, overshoot_enabled: bool = True,
-                        ignore_limits: bool = False, scope:Lumascope = Depends(get_scope)):
-    scope.move_absolute_position(axis, pos, False, overshoot_enabled, ignore_limits)
-    success = await scope.async_wait_until_finished_moving()
-    if not success:
-        raise HTTPException(status_code=500, detail="Could not complete the operation")
-    return {"message":"Movement completed"}
+class MotionType(str, Enum):
+    ABSOLUTE = "absolute"
+    RELATIVE = "relative"
 
-@motion_router.post("/relative")
-async def move_relative(axis: Axis, um: float, overshoot_enabled: bool = True, scope:Lumascope = Depends(get_scope)):
-    scope.move_relative_position(axis, um, False, overshoot_enabled)
+class MotionParameters(BaseModel):
+    motion_type: MotionType
+    axis: Axis
+    um: float
+    overshoot_enabled: bool = True
+    ignore_limits: bool = False
+
+@motion_router.post("")
+async def move(motion_parameters: MotionParameters, scope:Lumascope = Depends(get_scope)):
+    if motion_parameters.motion_type == MotionType.ABSOLUTE:
+        scope.move_absolute_position(axis = motion_parameters.axis,
+                                     pos = motion_parameters.um,
+                                     wait_until_complete = False,
+                                     overshoot_enabled = motion_parameters.overshoot_enabled,
+                                     ignore_limits = motion_parameters.ignore_limits)
+    elif motion_parameters.motion_type == MotionType.RELATIVE:
+        scope.move_relative_position(axis = motion_parameters.axis,
+                                     um = motion_parameters.um,
+                                     wait_until_complete = False,
+                                     overshoot_enabled = motion_parameters.overshoot_enabled)
     success = await scope.async_wait_until_finished_moving()
     if not success:
         raise HTTPException(status_code=500, detail="Could not complete the operation")
